829b0ce739534133c9abac9cca32dd7e48cd662e
webdmitriev <webdmitriev@gmail.com>
2025-11-14 12:54:49 +0300

    block comparison is ready, added slick slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  console.log('App start');
+  $('.comparison-slider').each(function () {
+    const slider = $(this);
+    slider.slick({
+      autoplay: true,
+      autoplaySpeed: 700000,
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      appendArrows: false,
+      rows: 0,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            centerMode: true,
+          }
+        }
+      ]
+    });
+
+    const sLightbox = $(this);
+    sLightbox.slickLightbox({
+      src: 'src',
+      itemSelector: 'img',
+      navigateByKeyboard: true
+    });
+  })
 
 });
