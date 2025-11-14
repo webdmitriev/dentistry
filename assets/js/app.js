9f03bfd8ddc8f7bed8b568e5e20d1ecc9859a993
webdmitriev <webdmitriev@gmail.com>
2025-11-14 14:51:24 +0300

    block faq is ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -122,4 +122,19 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   })
 
+  // ***
+  // faq
+  $(".panel-heading").click(function (e) {
+    $(this)
+      .toggleClass("in")
+      .next()
+      .slideToggle();
+    $(".panel-heading")
+      .not(this)
+      .removeClass("in")
+      .next()
+      .slideUp();
+    e.preventDefault();
+  });
+
 });
